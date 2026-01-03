3fe5594e611b5eac76d1272361111472461a06b8
abrenzink <a.brenzink@gmail.com>
2026-01-03 12:08:16 -0300

    feat: calculate days remaining until christmas

--- MyConsoleApp/Program.cs
+++ MyConsoleApp/Program.cs
@@ -1,4 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine($"The current time is {DateTime.Now}");
-Console.WriteLine($"The current time is {DateTime.Now}");
+
+DateTime today = DateTime.Today;
+DateTime targetDate = new DateTime(2026, 12, 5);
+
+int daysRemaining = (targetDate - today).Days;
+
+Console.WriteLine($"There are {daysRemaining} days until the next Christmas.");
+
--- MyConsoleApp/obj/Debug/net10.0/MyConsoleApp.AssemblyInfo.cs
+++ MyConsoleApp/obj/Debug/net10.0/MyConsoleApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyConsoleApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e420803d6b0ef021fcd127e10c5defce4c268e91")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyConsoleApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyConsoleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
